f2ae5c562e390b313ff0ae485e0a3bf6e0e94903
Andrea D'Amico <andrea.damico@gmail.com>
2014-05-07 13:36:45 +0200

    can specify encoding for tempfile, defaults ascii/8bit

--- lib/doc_raptor.rb
+++ lib/doc_raptor.rb
@@ -33,7 +33,8 @@ class DocRaptor
       :document_type              => "pdf",
       :test                       => false,
       :async                      => false,
-      :raise_exception_on_failure => false
+      :raise_exception_on_failure => false,
+      :encoding => "ascii-8bit"#TODO: add tests for this option
     }
     options = default_options.merge(options)
     raise_exception_on_failure = options[:raise_exception_on_failure]
@@ -61,7 +62,8 @@ class DocRaptor
 
     if block_given?
       ret_val = nil
-      Tempfile.open("docraptor") do |f|
+      Tempfile.open("docraptor", :encoding => options[:encoding]) do |f|
+
         f.sync = true
         f.write(response.body)
         f.rewind
